a19899ff6e02a69881ffac08813cd7b50173024f
zhang hailong <hailongz@qq.com>
2016-10-27 18:19:10 +0800

    update android-aar

--- android-aar/run.sh
+++ android-aar/run.sh
@@ -99,7 +99,7 @@ if [ -n "$GIT" ]; then
 		mkdir "$MAVEN_DIR/$MAVEN_GROUPID/$MAVEN_ARTIFACTID/$TAG"
 	fi
 
-	CMD="cp ./$AAR_NAME/build/outputs/aar/$AAR_FNAME $MAVEN_DIR/$MAVEN_GROUPID/$MAVEN_ARTIFACTID/$TAG/$AAR_NAME-$TAG.aar"
+	CMD="cp ./$AAR_NAME/build/outputs/aar/$AAR_FNAME $MAVEN_DIR/$MAVEN_GROUPID/$MAVEN_ARTIFACTID/$TAG/$MAVEN_ARTIFACTID-$TAG.aar"
 	runCommand
 
 	echo "[OK] TAG: $TAG"
